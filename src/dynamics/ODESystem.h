00161cf821cd7d8419daf44a3c6826dcf33d5439
Christopher Sconyers <48865772+chrissconyers@users.noreply.github.com>
2019-11-28 22:21:18 +0000

    clean up dynamics lib a bit

--- src/dynamics/ODESystem.h
+++ src/dynamics/ODESystem.h
@@ -11,6 +11,6 @@ namespace nereid
     {
     public:
         virtual ~ODESystem(void) {}
-        virtual State f(const State& x) = 0;
+        virtual State f(const State& x) const = 0;
     };
 }
